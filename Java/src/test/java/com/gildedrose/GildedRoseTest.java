3d201608fadc419012b4ca3873c3866dcfef8f99
Mark Bonicillo <mark.a.bonicillo@gmail.com>
2019-10-15 12:03:22 -0700

    Add test to check Quality is not negative

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,18 +1,45 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    private  Item[] items;
+    private GildedRose gildedRose;
+
+    @BeforeEach
+    void setUp() {
+        items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        gildedRose = new GildedRose(items);
     }
 
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    public void checkItemQualityNotNegative() {
+        for (Item item: gildedRose.items
+             ) {
+            assertTrue(item.quality > -1, "The Quality of an item is never negative: " + item.toString());
+        }
+    }
 }
